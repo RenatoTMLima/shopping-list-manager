a9ad3e6cc630e8c53df7b8ce9308916db5a5332c
rmvortx <49873623+RenatoTMLima@users.noreply.github.com>
2021-07-03 13:24:01 -0300

    Initial setup fo the project

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
+
+//Imports de componentes de página aqui
+
+const Routes: React.FC = () => (
+  <Switch>
+    {/* <Route path="/componente" component={Componente} /> */}
+  </Switch>
+);
+
+export default Routes;
